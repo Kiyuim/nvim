b0d8fa861a87431aab9c8bbd35a84efdac63385e
Kiyu <149651902+Kiyuim@users.noreply.github.com>
2024-03-29 22:17:41 +0800

    Update competitest.lua for mac os

--- lua/plugins/competitest.lua
+++ lua/plugins/competitest.lua
@@ -11,8 +11,8 @@ return{
         map("n", "<leader>ra", ":CompetiTest add_testcase<CR>", opt)
         map("n", "<leader>rd", ":CompetiTest delete_testcase<CR>", opt)
         map("n", "<leader>rt", ":CompetiTest receive testcases<CR>", opt)
-        --map("n", "<leader>rm", ":!rm -f %< %<_* <CR>", opt)
-        map("n", "<leader>rm", ":!del %<.exe %<_*.txt <CR>", opt)
+        map("n", "<leader>rm", ":!rm -f %< %<_* <CR>", opt)
+        --map("n", "<leader>rm", ":!del %<.exe %<_*.txt <CR>", opt)
         compile_command = {
             cpp       = { exec = 'clang++',           args = {'$(FNAME)', '-o', '$(FNOEXT)'}, '-std=c++2a' },
             some_lang = { exec = 'some_compiler', args = {'$(FNAME)'} },
